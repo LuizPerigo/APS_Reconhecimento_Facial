273e24faccff5b4e827fd32671da9db159d67f97
Vinicius Santos <vini_feliperodrigues@hotmail.com>
2021-11-09 09:56:17 -0300

    Funcao para abrir selecao de arquivos (imagens)

--- fileControl.py
+++ fileControl.py
@@ -1,5 +1,7 @@
 import shutil
 import os
+import tkinter as tk
+from tkinter import filedialog
 
 def createUserDir(usuario, imagePath):
     current_dir = os.getcwd()
@@ -8,3 +10,17 @@ def createUserDir(usuario, imagePath):
         os.makedirs(final_dir)
 
     shutil.copy2(imagePath, final_dir)
+
+def openImageSelector():
+    #TK cria uma UI para abrir o file explorer, com o codigo abaixo essa janela é oculta
+    root = tk.Tk()
+    root.withdraw()
+
+    #Abre explorador de arquivos
+    image_path = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Selecione uma imagem",
+                                          filetypes=[
+                                                    ('Imagens', ('.jpg', '.jpeg')),
+                                                    ('Todos os arquivos', '.*')
+                                                ])
+    return image_path
